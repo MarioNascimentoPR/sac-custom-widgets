520ed7fd14fe788073102f0c8029941c6cbe3a3a
MarioNascimentoPR <mario.nascimento@petroreconcavo.com.br>
2026-05-18 01:37:04 -0300

    Update Project_Summary.js

--- Project_Summary.js
+++ Project_Summary.js
@@ -609,7 +609,7 @@
       };
 
       // Teto fixo (passa folgado acima dos números). O Chão é o final da div do gráfico.
-      const ceilingY = 22;
+      const ceilingY = 10;
       const floorY = containerHeight; 
       
       pairs.forEach((pair) => {
