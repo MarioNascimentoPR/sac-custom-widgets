f6faf5622f2e86b4f70ef5d6b406fb32204b25ea
MarioNascimentoPR <mario.nascimento@petroreconcavo.com.br>
2026-05-19 10:38:34 -0300

    Update Project_Summary.js

--- Project_Summary.js
+++ Project_Summary.js
@@ -2415,7 +2415,7 @@
         const snapshot = await this._captureWidgetPng();
         this._exportPptBtn.disabled = true;
         this._setText(this._exportPptBtn, "Gerando PPT...");
-        const pptBlob = this._buildPptxBlob(snapshot.dataUrl, snapshot.width, snapshot.height);
+        const pptBlob = this._buildScreenshotPptxBlob(snapshot.dataUrl, snapshot.width, snapshot.height);
         const url = URL.createObjectURL(pptBlob);
         const anchor = document.createElement("a");
         anchor.href = url;
@@ -2479,6 +2479,180 @@
       ctx.scale(scale, scale);
       ctx.drawImage(image, 0, 0, width, height);
       return { dataUrl: canvas.toDataURL("image/png"), width, height };
+    }
+
+    _buildScreenshotPptxBlob(pngDataUrl, width, height) {
+      const encoder = new TextEncoder();
+      const pngBytes = this._base64ToBytes(pngDataUrl.split(",")[1]);
+      const slideWidth = 12192000;
+      const slideHeight = 6858000;
+      const imageScale = Math.min(slideWidth / Math.max(width, 1), slideHeight / Math.max(height, 1));
+      const imageWidth = Math.round(width * imageScale);
+      const imageHeight = Math.round(height * imageScale);
+      const imageX = Math.round((slideWidth - imageWidth) / 2);
+      const imageY = Math.round((slideHeight - imageHeight) / 2);
+      const now = new Date().toISOString();
+      const xml = (text) => encoder.encode(text.trim());
+      const entries = [
+        {
+          path: "[Content_Types].xml",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+              <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+              <Default Extension="xml" ContentType="application/xml"/>
+              <Default Extension="png" ContentType="image/png"/>
+              <Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/>
+              <Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/>
+              <Override PartName="/ppt/presentation.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml"/>
+              <Override PartName="/ppt/slides/slide1.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.slide+xml"/>
+              <Override PartName="/ppt/slideLayouts/slideLayout1.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.slideLayout+xml"/>
+              <Override PartName="/ppt/slideMasters/slideMaster1.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.slideMaster+xml"/>
+              <Override PartName="/ppt/theme/theme1.xml" ContentType="application/vnd.openxmlformats-officedocument.theme+xml"/>
+            </Types>`)
+        },
+        {
+          path: "_rels/.rels",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+              <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="ppt/presentation.xml"/>
+              <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties" Target="docProps/core.xml"/>
+              <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties" Target="docProps/app.xml"/>
+            </Relationships>`)
+        },
+        {
+          path: "docProps/app.xml",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+              <Application>Performance Summary Widget</Application>
+              <PresentationFormat>On-screen Show (16:9)</PresentationFormat>
+              <Slides>1</Slides>
+              <HiddenSlides>0</HiddenSlides>
+              <MMClips>0</MMClips>
+              <ScaleCrop>false</ScaleCrop>
+              <LinksUpToDate>false</LinksUpToDate>
+              <SharedDoc>false</SharedDoc>
+              <HyperlinksChanged>false</HyperlinksChanged>
+              <AppVersion>16.0000</AppVersion>
+            </Properties>`)
+        },
+        {
+          path: "docProps/core.xml",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+              <dc:title>Performance Summary</dc:title>
+              <dc:creator>Performance Summary Widget</dc:creator>
+              <cp:lastModifiedBy>Performance Summary Widget</cp:lastModifiedBy>
+              <dcterms:created xsi:type="dcterms:W3CDTF">${now}</dcterms:created>
+              <dcterms:modified xsi:type="dcterms:W3CDTF">${now}</dcterms:modified>
+            </cp:coreProperties>`)
+        },
+        {
+          path: "ppt/presentation.xml",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <p:presentation xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main">
+              <p:sldMasterIdLst><p:sldMasterId id="2147483648" r:id="rId1"/></p:sldMasterIdLst>
+              <p:sldIdLst><p:sldId id="256" r:id="rId2"/></p:sldIdLst>
+              <p:sldSz cx="${slideWidth}" cy="${slideHeight}" type="wide"/>
+              <p:notesSz cx="6858000" cy="9144000"/>
+            </p:presentation>`)
+        },
+        {
+          path: "ppt/_rels/presentation.xml.rels",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+              <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideMaster" Target="slideMasters/slideMaster1.xml"/>
+              <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slide" Target="slides/slide1.xml"/>
+            </Relationships>`)
+        },
+        {
+          path: "ppt/slides/slide1.xml",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <p:sld xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main">
+              <p:cSld name="Print da tela">
+                <p:bg><p:bgPr><a:solidFill><a:srgbClr val="FFFFFF"/></a:solidFill><a:effectLst/></p:bgPr></p:bg>
+                <p:spTree>
+                  <p:nvGrpSpPr><p:cNvPr id="1" name=""/><p:cNvGrpSpPr/><p:nvPr/></p:nvGrpSpPr>
+                  <p:grpSpPr><a:xfrm><a:off x="0" y="0"/><a:ext cx="0" cy="0"/><a:chOff x="0" y="0"/><a:chExt cx="0" cy="0"/></a:xfrm></p:grpSpPr>
+                  <p:pic>
+                    <p:nvPicPr><p:cNvPr id="2" name="Print da tela do widget"/><p:cNvPicPr><a:picLocks noChangeAspect="1"/></p:cNvPicPr><p:nvPr/></p:nvPicPr>
+                    <p:blipFill><a:blip r:embed="rId2"/><a:stretch><a:fillRect/></a:stretch></p:blipFill>
+                    <p:spPr>
+                      <a:xfrm><a:off x="${imageX}" y="${imageY}"/><a:ext cx="${imageWidth}" cy="${imageHeight}"/></a:xfrm>
+                      <a:prstGeom prst="rect"><a:avLst/></a:prstGeom>
+                    </p:spPr>
+                  </p:pic>
+                </p:spTree>
+              </p:cSld>
+              <p:clrMapOvr><a:masterClrMapping/></p:clrMapOvr>
+            </p:sld>`)
+        },
+        {
+          path: "ppt/slides/_rels/slide1.xml.rels",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+              <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideLayout" Target="../slideLayouts/slideLayout1.xml"/>
+              <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="../media/image1.png"/>
+            </Relationships>`)
+        },
+        {
+          path: "ppt/slideMasters/slideMaster1.xml",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <p:sldMaster xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main">
+              <p:cSld><p:spTree><p:nvGrpSpPr><p:cNvPr id="1" name=""/><p:cNvGrpSpPr/><p:nvPr/></p:nvGrpSpPr><p:grpSpPr/></p:spTree></p:cSld>
+              <p:clrMap bg1="lt1" tx1="dk1" bg2="lt2" tx2="dk2" accent1="accent1" accent2="accent2" accent3="accent3" accent4="accent4" accent5="accent5" accent6="accent6" hlink="hlink" folHlink="folHlink"/>
+              <p:sldLayoutIdLst><p:sldLayoutId id="2147483649" r:id="rId1"/></p:sldLayoutIdLst>
+              <p:txStyles><p:titleStyle/><p:bodyStyle/><p:otherStyle/></p:txStyles>
+            </p:sldMaster>`)
+        },
+        {
+          path: "ppt/slideMasters/_rels/slideMaster1.xml.rels",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+              <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideLayout" Target="../slideLayouts/slideLayout1.xml"/>
+              <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme" Target="../theme/theme1.xml"/>
+            </Relationships>`)
+        },
+        {
+          path: "ppt/slideLayouts/slideLayout1.xml",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <p:sldLayout xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main" type="blank" preserve="1">
+              <p:cSld name="Blank"><p:spTree><p:nvGrpSpPr><p:cNvPr id="1" name=""/><p:cNvGrpSpPr/><p:nvPr/></p:nvGrpSpPr><p:grpSpPr/></p:spTree></p:cSld>
+              <p:clrMapOvr><a:masterClrMapping/></p:clrMapOvr>
+            </p:sldLayout>`)
+        },
+        {
+          path: "ppt/slideLayouts/_rels/slideLayout1.xml.rels",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+              <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideMaster" Target="../slideMasters/slideMaster1.xml"/>
+            </Relationships>`)
+        },
+        {
+          path: "ppt/theme/theme1.xml",
+          data: xml(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <a:theme xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" name="Office Theme">
+              <a:themeElements>
+                <a:clrScheme name="Office">
+                  <a:dk1><a:sysClr val="windowText" lastClr="000000"/></a:dk1><a:lt1><a:sysClr val="window" lastClr="FFFFFF"/></a:lt1>
+                  <a:dk2><a:srgbClr val="1F497D"/></a:dk2><a:lt2><a:srgbClr val="EEECE1"/></a:lt2>
+                  <a:accent1><a:srgbClr val="4F81BD"/></a:accent1><a:accent2><a:srgbClr val="C0504D"/></a:accent2><a:accent3><a:srgbClr val="9BBB59"/></a:accent3>
+                  <a:accent4><a:srgbClr val="8064A2"/></a:accent4><a:accent5><a:srgbClr val="4BACC6"/></a:accent5><a:accent6><a:srgbClr val="F79646"/></a:accent6>
+                  <a:hlink><a:srgbClr val="0000FF"/></a:hlink><a:folHlink><a:srgbClr val="800080"/></a:folHlink>
+                </a:clrScheme>
+                <a:fontScheme name="Office"><a:majorFont><a:latin typeface="Arial"/><a:ea typeface=""/><a:cs typeface=""/></a:majorFont><a:minorFont><a:latin typeface="Arial"/><a:ea typeface=""/><a:cs typeface=""/></a:minorFont></a:fontScheme>
+                <a:fmtScheme name="Office">
+                  <a:fillStyleLst><a:solidFill><a:schemeClr val="phClr"/></a:solidFill><a:solidFill><a:schemeClr val="phClr"/></a:solidFill><a:solidFill><a:schemeClr val="phClr"/></a:solidFill></a:fillStyleLst>
+                  <a:lnStyleLst><a:ln w="9525"><a:solidFill><a:schemeClr val="phClr"/></a:solidFill></a:ln><a:ln w="25400"><a:solidFill><a:schemeClr val="phClr"/></a:solidFill></a:ln><a:ln w="38100"><a:solidFill><a:schemeClr val="phClr"/></a:solidFill></a:ln></a:lnStyleLst>
+                  <a:effectStyleLst><a:effectStyle><a:effectLst/></a:effectStyle><a:effectStyle><a:effectLst/></a:effectStyle><a:effectStyle><a:effectLst/></a:effectStyle></a:effectStyleLst>
+                  <a:bgFillStyleLst><a:solidFill><a:schemeClr val="phClr"/></a:solidFill><a:solidFill><a:schemeClr val="phClr"/></a:solidFill><a:solidFill><a:schemeClr val="phClr"/></a:solidFill></a:bgFillStyleLst>
+                </a:fmtScheme>
+              </a:themeElements>
+              <a:objectDefaults/><a:extraClrSchemeLst/>
+            </a:theme>`)
+        },
+        { path: "ppt/media/image1.png", data: pngBytes }
+      ];
+      return new Blob([this._zipEntries(entries)], { type: "application/vnd.openxmlformats-officedocument.presentationml.presentation" });
     }
 
     _buildPptxBlob(pngDataUrl, width, height) {
@@ -2693,19 +2867,22 @@
       const encoder = new TextEncoder();
       const localFiles = [];
       const centralFiles = [];
+      const now = new Date();
+      const dosTime = (now.getHours() << 11) | (now.getMinutes() << 5) | Math.floor(now.getSeconds() / 2);
+      const dosDate = ((Math.max(now.getFullYear(), 1980) - 1980) << 9) | ((now.getMonth() + 1) << 5) | now.getDate();
       let offset = 0;
       entries.forEach(entry => {
         const nameBytes = encoder.encode(entry.path);
         const data = entry.data instanceof Uint8Array ? entry.data : new Uint8Array(entry.data);
         const crc = this._crc32(data);
         const localHeader = this._concatBytes([
-          this._u32(0x04034b50), this._u16(20), this._u16(0), this._u16(0), this._u16(0), this._u16(0),
+          this._u32(0x04034b50), this._u16(20), this._u16(0), this._u16(0), this._u16(dosTime), this._u16(dosDate),
           this._u32(crc), this._u32(data.length), this._u32(data.length), this._u16(nameBytes.length), this._u16(0), nameBytes
         ]);
         localFiles.push(localHeader, data);
 
         const centralHeader = this._concatBytes([
-          this._u32(0x02014b50), this._u16(20), this._u16(20), this._u16(0), this._u16(0), this._u16(0), this._u16(0),
+          this._u32(0x02014b50), this._u16(20), this._u16(20), this._u16(0), this._u16(0), this._u16(dosTime), this._u16(dosDate),
           this._u32(crc), this._u32(data.length), this._u32(data.length), this._u16(nameBytes.length), this._u16(0), this._u16(0),
           this._u16(0), this._u16(0), this._u32(0), this._u32(offset), nameBytes
         ]);
